14be81cb7a8ec70fee8adf652005f4fd28d7e11f
CELAL KUTLUER <celalkutluer@gmail.com>
2020-03-05 00:36:58 +0300

    düzenlemeler

--- inc/header.php
+++ inc/header.php
@@ -20,7 +20,7 @@
 
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand" href="index.php">BORSA</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
+        <button class="navbar-toggler " type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
@@ -37,6 +37,7 @@
             <ul class="nav navbar-nav navbar-right ml-auto">
                 <li class="nav-item"><a href="giris.php" class="nav-link">Giriş</a></li>
                 <li class="nav-item"><a href="kayit.php" class="nav-link">Kayıt</a></li>
+                <li class="nav-item"><a href="profil.php" class="nav-link">Profilim</a></li>
             </ul>
         </div>
     </nav>
