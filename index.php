295c40f9942c7f4d64a3cebea84d02e47c1a1fb0
CELAL KUTLUER <celalkutluer@gmail.com>
2020-03-05 00:12:38 +0300

    index dosyası düzenlemeleri

--- index.php
+++ index.php
@@ -3,53 +3,193 @@ include "settings/baglantilar.php";
 //include "settings/fonksiyonlar.php";
 include "inc/header.php";
 ?>
-
-
-    <div class="table">
-        <table class="table table-bordered">
-            <thead>
-            <tr>
-                <th scope="col">Sembol</th>
-                <th scope="col">Şirket</th>
-                <th scope="col">Durum</th>
-                <th scope="col">Fiyat</th>
-                <th scope="col">(%)Fark</th>
-                <th scope="col">Zaman</th>
-                <th scope="col">Alış</th>
-                <th scope="col">Satış</th>
-            </tr>
-            </thead>
-            <tbody>
-            <?php
-            function hisse()
-            {
-                Global $db;
-                $veri = $db->prepare("SELECT * FROM borsa_bilgi 
-LEFT JOIN borsa_anlik ON borsa_bilgi.Sembol=borsa_anlik.Sembol");
-                $veri->execute(array());
-                $v = $veri->fetchAll(pdo::FETCH_ASSOC);
-                $say = $veri->rowCount();
-                if ($say) {
-                    foreach ($v as $tum_hisseler) {
-                        ?>
+    <span class="col-lg"></span>
+    <div class="conteiner ">
+        <div class=" row ">
+            <div class="col-sm-6">
+                <div class="table col-sm">
+                    <table class="table table-bordered bg-light">
+                        <span class="h6">En Çok Yükselenler</span>
+                        <thead>
                         <tr>
-                            <td><?php echo $tum_hisseler['Sembol']; ?></td>
-                            <td><?php echo $tum_hisseler['Tam_Ad']; ?></td>
-                            <td>*</td>
-                            <td><?php echo $tum_hisseler['Fiyat']; ?></td>
-                            <td>*</td>
-                            <td><?php echo $tum_hisseler['Zaman']; ?></td>
-                            <td><button type="button" class="btn btn-success">ALIŞ</button></td>
-                            <td><button type="button" class="btn btn-danger">SATIŞ</button></td>
+                            <th scope="col">Sembol</th>
+                            <th scope="col">Fiyat</th>
+                            <th scope="col">(%)Fark</th>
                         </tr>
-                        <?php
+                        </thead>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+            <div class="col-sm-6">
+                <div class="table col-sm">
+                    <table class="table table-bordered bg-light">
+                        <span class="h6">En Çok Düşenler</span>
+                        <thead>
+                        <tr>
+                            <th scope="col">Sembol</th>
+                            <th scope="col">Fiyat</th>
+                            <th scope="col">(%)Fark</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                        <tbody>
+                        <td>*</td>
+                        <td>*</td>
+                        <td>*</td>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+<span class="col-md"></span>
+    <div class="conteiner col-sm-12">
+        <div class=" row ">
+            <div class="table col-sm">
+                <table class="table table-bordered bg-light">
+                    <thead>
+                    <tr>
+                        <th scope="col">Sembol</th>
+                        <th scope="col">Şirket</th>
+                        <th scope="col">Durum</th>
+                        <th scope="col">Fiyat</th>
+                        <th scope="col">(%)Fark</th>
+                        <th scope="col">Zaman</th>
+                        <th scope="col">Alış</th>
+                        <th scope="col">Satış</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <?php
+                    function hisse()
+                    {
+                        Global $db;
+                        $veri = $db->prepare("SELECT * FROM borsa_bilgi LEFT JOIN borsa_anlik ON borsa_bilgi.Sembol=borsa_anlik.Sembol");
+                        $veri->execute(array());
+                        $v = $veri->fetchAll(pdo::FETCH_ASSOC);
+                        $say = $veri->rowCount();
+                        if ($say) {
+                            foreach ($v as $tum_hisseler) {
+                                ?>
+                                <tr>
+                                    <td><?php echo $tum_hisseler['Sembol']; ?></td>
+                                    <td><?php echo $tum_hisseler['Tam_Ad']; ?></td>
+                                    <td>*</td>
+                                    <td><?php echo $tum_hisseler['Fiyat']; ?></td>
+                                    <td>*</td>
+                                    <td><?php echo $tum_hisseler['Zaman']; ?></td>
+                                    <td>
+                                        <button type="button" class="btn btn-success">ALIŞ</button>
+                                    </td>
+                                    <td>
+                                        <button type="button" class="btn btn-danger">SATIŞ</button>
+                                    </td>
+                                </tr>
+                                <?php
+                            }
+                        }
                     }
-                }
-            }
-            hisse();
-            ?>
-            </tbody>
-        </table>
+
+                    hisse();
+                    ?>
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 
 
