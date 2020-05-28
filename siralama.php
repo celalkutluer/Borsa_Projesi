962b3be0a2d68d57177a646ecd5b5f3b0887e9d1
CELAL KUTLUER <celalkutluer@gmail.com>
2020-05-28 11:34:42 +0300

    siralama.php lig sıralaması tamamlandı

--- siralama.php
+++ siralama.php
@@ -171,7 +171,8 @@ WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 MONTH), INTERV
                                                     $veri_gunluk_kul_sira = $db->prepare('
 SELECT UPPER(kullanicilar.kul_Ad) AS AD,UPPER(kullanicilar.kul_Soyad) AS SOYAD, SUM(satim.satim_kar_zarar) AS DURUM FROM satim 
 INNER JOIN kullanicilar ON kullanicilar.kul_Id = satim.satim_kul_id 
-WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), INTERVAL -1 DAY) and DATE_SUB(CURDATE(), INTERVAL -1 DAY) GROUP BY satim_kul_id ORDER BY satim_kar_zarar DESC LIMIT 10');
+WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), INTERVAL -1 DAY) and DATE_SUB(CURDATE(), INTERVAL -1 DAY) 
+GROUP BY satim_kul_id ORDER BY satim_kar_zarar DESC LIMIT 10');
                                                     $veri_gunluk_kul_sira->execute(array());
                                                     $v_gunluk_kul_sira = $veri_gunluk_kul_sira->fetchAll(PDO::FETCH_ASSOC);
                                                     $say_gunluk_kul_sira = $veri_gunluk_kul_sira->rowCount();
@@ -219,22 +220,26 @@ WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), INTERVA
                                                     <thead>
                                                     <tr>
                                                         <th class="text-center">Sıra</th>
-                                                        <th class="text-center">Ad</th>
-                                                        <th class="text-center">Soyad</th>
+                                                        <th class="text-center">Lig Adı</th>
                                                         <th class="text-center">Kazanç</th>
                                                     </tr>
                                                     </thead>
                                                     <tbody>
                                                     <?php
-                                                    for ($sayi = 0; $sayi < 10; $sayi++) {
+                                                    $sayi=0;
+                                                    $veri_lig_hafta_sira = $db->prepare('SELECT UPPER(ligler.lig_baslik) AS LIG, SUM(satim.satim_kar_zarar) AS DURUM FROM satim INNER JOIN kullanicilar ON kullanicilar.kul_Id = satim.satim_kul_id INNER JOIN ligler ON ligler.lig_id = kullanicilar.kul_lig_id WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 WEEK), INTERVAL -1 DAY) and DATE_SUB(CURDATE(), INTERVAL -1 DAY) GROUP BY ligler.lig_id ORDER BY durum DESC LIMIT 10');
+                                                    $veri_lig_hafta_sira->execute(array());
+                                                    $v_lig_hafta_sira = $veri_lig_hafta_sira->fetchAll(PDO::FETCH_ASSOC);
+                                                    $say_lig_hafta_sira = $veri_lig_hafta_sira->rowCount();
+                                                    foreach ($v_lig_hafta_sira as $lig_hafta_sira) {
                                                         echo
                                                             "<tr>
-                                                        <td class='text-center'>" . ($sayi + 1) . "</td>
-                                                        <td class='text-center' id='kazananlar_gun_Ad_" . $sayi . "'>Ad</td>
-                                                        <td class='text-center' id='kazananlar_gun_Soyad_" . $sayi . "'>Soyad</td>
-                                                        <td class='text-center' id='kazananlar_gun_TL_" . $sayi . "'>TL</td>
-                                                    </tr> 
-                                                ";
+                                                        <td class='text-center'>" . ($sayi+1) . "</td>
+                                                        <td class='text-center' id='lig_hafta_Ad_" . $sayi . "'>" . $lig_hafta_sira['LIG']. "</td>
+                                                        <td class='text-center' id='lig_hafta_TL_" . $sayi . "'>" . $lig_hafta_sira['DURUM']. " TL</td>
+                                                                </tr> 
+                                                                ";
+                                                        $sayi++;
                                                     }
                                                     ?>
                                                     </tbody>
@@ -251,22 +256,26 @@ WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), INTERVA
                                                     <thead>
                                                     <tr>
                                                         <th class="text-center">Sıra</th>
-                                                        <th class="text-center">Ad</th>
-                                                        <th class="text-center">Soyad</th>
+                                                        <th class="text-center">Lig Adı</th>
                                                         <th class="text-center">Kazanç</th>
                                                     </tr>
                                                     </thead>
                                                     <tbody>
                                                     <?php
-                                                    for ($sayi = 0; $sayi < 10; $sayi++) {
+                                                    $sayi=0;
+                                                    $veri_lig_ay_sira = $db->prepare('SELECT UPPER(ligler.lig_baslik) AS LIG, SUM(satim.satim_kar_zarar) AS DURUM FROM satim INNER JOIN kullanicilar ON kullanicilar.kul_Id = satim.satim_kul_id INNER JOIN ligler ON ligler.lig_id = kullanicilar.kul_lig_id WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 MONTH), INTERVAL -1 DAY) and DATE_SUB(CURDATE(), INTERVAL -1 DAY) GROUP BY ligler.lig_id ORDER BY durum DESC LIMIT 10');
+                                                    $veri_lig_ay_sira->execute(array());
+                                                    $v_lig_ay_sira = $veri_lig_ay_sira->fetchAll(PDO::FETCH_ASSOC);
+                                                    $say_lig_ay_sira = $veri_lig_ay_sira->rowCount();
+                                                    foreach ($v_lig_ay_sira as $lig_ay_sira) {
                                                         echo
                                                             "<tr>
-                                                        <td class='text-center'>" . ($sayi + 1) . "</td>
-                                                        <td class='text-center' id='kazananlar_hafta_Ad_" . $sayi . "'>Ad</td>
-                                                        <td class='text-center' id='kazananlar_hafta_Soyad_" . $sayi . "'>Soyad</td>
-                                                        <td class='text-center' id='kazananlar_hafta_TL_" . $sayi . "'>TL</td>
+                                                        <td class='text-center'>" . ($sayi+1) . "</td>
+                                                        <td class='text-center' id='lig_ay_Ad_" . $sayi . "'>" . $lig_ay_sira['LIG']. "</td>
+                                                        <td class='text-center' id='lig_ay_TL_" . $sayi . "'>" . $lig_ay_sira['DURUM']. " TL</td>
                                                                 </tr> 
                                                                 ";
+                                                        $sayi++;
                                                     }
                                                     ?>
                                                     </tbody>
@@ -284,22 +293,26 @@ WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), INTERVA
                                                     <thead>
                                                     <tr>
                                                         <th class="text-center">Sıra</th>
-                                                        <th class="text-center">Ad</th>
-                                                        <th class="text-center">Soyad</th>
+                                                        <th class="text-center">Lig Adı</th>
                                                         <th class="text-center">Kazanç</th>
                                                     </tr>
                                                     </thead>
                                                     <tbody>
                                                     <?php
-                                                    for ($sayi = 0; $sayi < 10; $sayi++) {
+                                                    $sayi=0;
+                                                    $veri_lig_genel_sira = $db->prepare('SELECT UPPER(ligler.lig_baslik) AS LIG, SUM(satim.satim_kar_zarar) AS DURUM FROM satim INNER JOIN kullanicilar ON kullanicilar.kul_Id = satim.satim_kul_id INNER JOIN ligler ON ligler.lig_id = kullanicilar.kul_lig_id WHERE satim_zaman between DATE_SUB(DATE_SUB(CURDATE(), INTERVAL 1 YEAR), INTERVAL -1 DAY) and DATE_SUB(CURDATE(), INTERVAL -1 DAY) GROUP BY ligler.lig_id ORDER BY durum DESC LIMIT 10');
+                                                    $veri_lig_genel_sira->execute(array());
+                                                    $v_lig_genel_sira = $veri_lig_genel_sira->fetchAll(PDO::FETCH_ASSOC);
+                                                    $say_lig_genel_sira = $veri_lig_genel_sira->rowCount();
+                                                    foreach ($v_lig_genel_sira as $lig_genel_sira) {
                                                         echo
                                                             "<tr>
-                                                        <td class='text-center'>" . ($sayi + 1) . "</td>
-                                                        <td class='text-center' id='kazananlar_ay_Ad_" . $sayi . "'>Ad</td>
-                                                        <td class='text-center' id='kazananlar_ay_Soyad_" . $sayi . "'>Soyad</td>
-                                                        <td class='text-center' id='kazananlar_ay_TL_" . $sayi . "'>TL</td>
+                                                        <td class='text-center'>" . ($sayi+1) . "</td>
+                                                        <td class='text-center' id='lig_genel_Ad_" . $sayi . "'>" . $lig_genel_sira['LIG']. "</td>
+                                                        <td class='text-center' id='lig_genel_TL_" . $sayi . "'>" . $lig_genel_sira['DURUM']. " TL</td>
                                                                 </tr> 
                                                                 ";
+                                                        $sayi++;
                                                     }
                                                     ?>
                                                     </tbody>
