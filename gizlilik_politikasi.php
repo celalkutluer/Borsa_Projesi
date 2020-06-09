e08574c1e8f5b968ca8c178e90318e75d7f37e1c
CELAL KUTLUER <celalkutluer@gmail.com>
2020-06-09 17:52:00 +0300

    çerezler için uyarı eklendi. gizlilik politikası sayfası eklendi. phpmailler için gerekli dosyalar eklendi.

---
+++ gizlilik_politikasi.php
@@ -0,0 +1,87 @@
+<?php
+include "inc/header.php";
+?>
+<section role="main" class="content-body">
+    <section class="section bg-color-quy">
+        <section class="section bg-color-quaternary custom-padding-4 border-0 my-0">
+            <div class="col-md">
+            	<div class="row">
+            		<div class="arc-richtext-editor-wrapper col-xs-12">
+            			
+            				<p style="text-align: center;"><b>Çerez Aydınlatma Metni</b></p>
+            <p> </p>
+            <p>Borsa Yatırım Fantazi Ligi olarak, web sitelerimiz (“<b>Site</b>”), uygulamalarımız ya da dijital ortamda sizlerin kullanımına sunduğumuz benzeri tüm çevrimiçi ya da çevrimdışı mecralarımızı (anılan tüm mecralar birlikte “<b>Platform</b>” olarak anılacaktır.) kullanımınız veya ziyaretiniz sırasında sizlerin deneyimini geliştirmek için çerezler, pikseller, GIFler vb. birtakım teknolojilerden (“<b>çerezler</b>”) faydalanmaktayız. </p>
+            <p> </p>
+            <p>Bu teknolojilerin kullanımı başta 6698 sayılı Kişisel Verilerin Korunması Kanunu (“<b>KVK Kanunu</b>”) olmak üzere tabi olduğumuz mevzuata uygun şekilde gerçekleştirilmektedir.</p>
+            <p> </p>
+            <p>İşbu Çerez Aydınlatma<i> </i>Metni’nin amacı, Platformların kullanımı esnasında kullanılmakta olan çerez ve piksel gibi kişisel verilerin toplanması yoluyla kişisel verilerin işlenmesi nedeniyle elde edilen kişisel verilerin işlenmesine ilişkin olarak sizlere bilgi vermektir.  İşbu metinde sitemizde ve uygulamamızda hangi amaçlarla hangi tür çerezleri kullandığımızı ve bu çerezleri nasıl kontrol edebileceğinizi sizlere açıklamak istiyoruz.</p>
+            <p> </p>
+            <p>Borsa Yatırım Fantazi Ligi olarak sitemizde ve uygulamamızda kullandığımız çerezleri kullanmaktan vazgeçebilir, bunların türlerini veya fonksiyonlarını değiştirebilir veya sitemize ve uygulamamızda yeni çerezler ekleyebiliriz. Dolayısıyla işbu Çerez Aydınlatma Metni’nin hükümlerini dilediğimiz zaman değiştirme hakkını saklı tutarız. Güncel Çerez Aydınlatma Metni üzerinde gerçekleştirilmiş olan her türlü değişiklik sitede, uygulamada veya herhangi bir kamuya açık mecrada yayınlanmakla birlikte yürürlük kazanacaktır. Son güncelleme tarihini metnin sonunda bulabilirsiniz.</p>
+            <p> </p>
+            <p><b>Kişisel Veri Toplamanın Yöntemi ve Hukuki Sebebi</b></p>
+            <p>Kişisel verileriniz, internet sitemizi ziyaretiniz kapsamında veya uygulamamızı kullanmanız dolayısıyla elektronik ortamda çerezler yoluyla web sitemimizin meşru menfaatine yönelik hukuki sebebe dayalı olarak toplanmaktadır. Hedefleme ve profilleme vasıtasıyla gerçekleştirilen pazarlama ve reklamcılık faaliyetleri ise yalnızca açık rızanız olması halinde gerçekleştirilmektedir. Toplanan kişisel verileriniz Kanun’un 5. ve 6. maddelerinde belirtilen kişisel veri işleme şartları ve amaçları kapsamında işbu Çerez Aydınlatma Metni’nde belirtilen amaçlarla da işlenebilmektedir.</p>
+            <p><b>Kişisel Verilerin Kimlere ve Hangi Amaçla Aktarılabileceği</b></p>
+            <p>Borsa Yatırım Fantazi Ligi olarak, Çerez Aydınlatma Metni kapsamındaki kişisel verilerinizi yukarıda belirtilen amaçların gerçekleştirilebilmesi ile sınırlı olarak ve mevzuata uygun şekilde<a> </a> tedarikçilerimize, kanunen yetkili kamu kurumlarına ve özel kişilere paylaşabiliriz</p>
+            <p><b>Hangi Çerezler Hangi Amaçlarla Kullanılmaktadır?</b></p>
+            <p><b>Genel Olarak</b></p>
+            <p>Borsa Yatırım Fantazi Ligi olarak sitemizde çeşitli amaçlarla çerezler kullanmakta ve bu çerezler vasıtasıyla kişisel verilerinizi işlemekteyiz. Bu amaçlar başlıca şunlardır:</p>
+            <p>–       <b>Site’nin çalışması için gerekli temel fonksiyonları gerçekleştirmek.</b> Örneğin, oturum açan üyelerin Site’de farklı sayfaları ziyaret ederken tekrar şifre girmelerine gerek kalmaması.</p>
+            <p>–       <b>Site’yi analiz etmek ve Site’nin performansını arttırmak.</b> Örneğin, Site’nin üzerinde çalıştığı farklı sunucuların entegrasyonu, Site’yi<b> </b>ziyaret edenlerin sayısının tespit edilmesi ve buna göre performans ayarlarının yapılması ya da ziyaretçilerin aradıklarını bulmalarının kolaylaştırılması.</p>
+            <p>–       <b>Site’nin işlevselliğini arttırmak ve kullanım kolaylığı sağlamak. </b>Örneğin, Site üzerinden üçüncü taraf sosyal medya mecralarına paylaşımda bulunmak, Site’yi ziyaret eden ziyaretçinin daha sonraki ziyaretinde kullanıcı adı bilgisinin ya da arama sorgularının hatırlanması.</p>
+            <p>–       <b>Kişiselleştirme, hedefleme ve reklamcılık faaliyeti gerçekleştirmek. </b>Örneğin,<b> </b>ziyaretçilerin görüntüledikleri sayfa ve ürünler üzerinden ziyaretçilerin ilgi alanlarıyla bağlantılı reklam gösterilmesi. </p>
+            <p>KVK Kanunu’nun 5. ve 8. maddeleri uyarınca ve/veya ilgili mevzuattaki istisnaların varlığı halinde kişisel verileriniz mevzuat uyarınca gerekli olan hallerde rızanız doğrultusunda, aksi hallerde rızanız alınmaksızın yukarıdaki amaçlar doğrultusunda işlenebilecektir. Borsa Yatırım Fantazi Ligi olarak, aydınlatma metni kapsamındaki kişisel  verilerinizi yukarıda belirtilen amaçların gerçekleştirilebilmesi ile sınırlı olarak ve mevzuata uygun şekilde web sitemiz hizmetlerinden faydalandığı üçüncü kişilerle, bağlı ortaklıklarımızla, iş ortaklarımızla paylaşabiliriz. Verilerin aktarıldığı tarafların kişisel verilerinizi dünyanın her yerindeki sunucularında saklayabileceğini belirtmek isteriz.</p>
+            <p><b>Sitemizde ve Uygulamamızda Kullanılan Çerezler</b></p>
+            <p>Aşağıda sitemizde ve uygulamamızda kullandığımız farklı türdeki çerezleri bulabilirsiniz. <a></a>Sitemizde ve uygulamamızda hem birinci parti çerezler (ziyaret ettiğiniz site tarafından yerleştirilen) hem de üçüncü parti çerezleri (ziyaret ettiğiniz site haricindeki sunucular tarafından yerleştirilen) kullanılmaktadır. </p>
+            <p><b>Zorunlu Çerezler</b></p>
+            <p>Belli çerezlerin kullanımı sitemizin ve uygulamamızın doğru biçimde çalışması için zorunludur. Örneğin sitemizde oturum açtığınızda devreye giren kimlik doğrulama çerezleri, sitemizde bir sayfadan diğerine geçişinizde etkin olan oturumunuzun devam etmesini sağlamaktadır. </p>
+            <p><b>İşlevsellik</b> <b>ve Tercih Çerezleri</b></p>
+            <p>Bu çerezler sizlerin site ve uygulama üzerindeki tercihlerini ve seçimlerinizi hatırlayarak sitemizde sunulan hizmetlerin sizin için kişiselleşmesini sağlamaktadır. Örneğin sitemiz üzerindeki dil seçiminizi veya bir metin okurken seçmiş olduğunuz font boyutunu hatırlamamızı sağlar.</p>
+            <p><b>Sosyal Medya Çerezleri</b></p>
+            <p>Bu çerezler sizlerin sosyal medya kullanımlarınız hakkında bilgilerin toplanmasını sağlar. Örneğin Kişiselleştirilmiş reklamlar oluşturulması ya da market araştırmaları yapılması için Facebook/Twitter hesaplarınıza ait bilgilerin kullanılması için çerezler kullanılabilir.</p>
+            <p><b>Performans ve Analiz Çerezleri</b></p>
+            <p>Bu çerezler sayesinde sitemizi ve uygulamamızı kullanımınızı ve performans analizi yaparak sizlere verdiğimiz hizmetleri daha iyi hale getirebiliyoruz. Örneğin bu çerezler sayesinde ziyaretçilerimizin en çok hangi sayfaları görüntülediğini, sitemizin gerektiği gibi çalışıp çalışmadığını ve olası problemleri tespit edebiliyoruz. </p>
+            <p><b>Hedefleme veya reklam çerezleri</b></p>
+            <p>Sizlere sitemizde veya sitemiz haricindeki mecralarda ürün ve hizmet tanıtımını yapmak için çerezler kullanıyoruz. Ayrıca bazı iş ortaklarımızla sizlere sitemiz dahilinde veya dışında reklam ve tanıtım yapmak için iş birliğine gidebiliriz. Örneğin, sitemizde gördüğünüz bir reklama tıklayıp tıklamadığınızı, eğer reklam ilginizi çektikten sonra o reklam yönlendirdiği sitedeki hizmetten faydalanıp faydalanmadığınızı takip etmek için çerezler kullanılabilmektedir. </p>
+            <p><b>Çerezlerin Kullanımını Nasıl Kontrol Edebilirim?</b></p>
+            <p>Çerez ve benzeri teknolojilerin kullanımı konusunda ziyaretçi ve kullanıcılarımızın tercihleri bizler için esastır. Buna karşın, Platform’un çalışması için zorunlu olan Çerezler’in kullanılması gerekmektedir. Ek olarak bazı çerezlerin kapatılması halinde Platform’un birtakım işlevlerinin kısmen ya da tamamen çalışmayabileceğini hatırlatmak isteriz.</p>
+            <p> </p>
+            <p>Platform’da kullanılan çerezlere dair tercihlerinizi ne şekilde yönetebileceğinize ilişkin bilgiler aşağıdaki gibidir:  </p>
+            <p>·        Ziyaretçiler, Platform’u görüntüledikleri tarayıcı ayarlarını değiştirerek çerezlere ilişkin tercihlerini kişiselleştirme imkanına sahiptir. Eğer kullanılmakta olan tarayıcı bu imkânı sunmaktaysa, tarayıcı ayarları üzerinden çerezlere ilişkin tercihleri değiştirmek mümkündür. Böylelikle, tarayıcının sunmuş olduğu imkânlara göre farklılık gösterebilmekle birlikte, veri sahiplerinin çerezlerin kullanılmasını engelleme, çerez kullanılmadan önce uyarı almayı tercih etme veya sadece bazı çerezleri devre bırakma ya da silme imkanları bulunmaktadır.</p>
+            <p>·        Bu konudaki tercihler kullanılan tarayıcıya göre değişiklik göstermekle birlikte genel açıklamaya <a href="https://www.aboutcookies.org/" target="_blank">https://www.aboutcookies.org/</a>  adresinden ulaşmak mümkündür. Çerezlere ilişkin tercihlerin, ziyaretçinin Platform’a erişim sağladığı her bir cihaz özelinde ayrı ayrı yapılması gerekebilecektir. </p>
+            <p>·        Google Analytics tarafından yönetilen çerezleri kapatmak için <a href="https://tools.google.com/dlpage/gaoptout" target="_blank">tıklayınız</a>.</p>
+            <p>·        Google tarafından sağlanan kişiselleştirilmiş reklam deneyimini yönetmek için <a href="https://www.google.com/settings/ads/" target="_blank">tıklayınız</a>. </p>
+            
+            <p> </p>
+            <p><b>Veri Sahibi Olarak Haklarınız Nelerdir?</b></p>
+            <p>KVK Kanunu’nun 11. maddesi uyarınca veri sahipleri,</p>
+            <p>·        kişisel veri işlenip işlenmediğini öğrenme,</p>
+            <p>·        kişisel verileri işlenmişse buna ilişkin bilgi talep etme,</p>
+            <p>·        kişisel verilerin işlenme amacını ve bunların amacına uygun kullanılıp kullanılmadığını öğrenme,</p>
+            <p>·        yurt içinde veya yurt dışında kişisel verilerin aktarıldığı üçüncü kişileri bilme,</p>
+            <p>·        kişisel verilerin eksik veya yanlış işlenmiş olması hâlinde bunların düzeltilmesini isteme ve bu kapsamda yapılan işlemin kişisel verilerin aktarıldığı üçüncü kişilere bildirilmesini isteme,</p>
+            <p>·        KVK Kanunu ve ilgili diğer kanun hükümlerine uygun olarak işlenmiş olmasına rağmen, işlenmesini gerektiren sebeplerin ortadan kalkması hâlinde kişisel verilerin silinmesini veya yok edilmesini isteme ve bu kapsamda yapılan işlemin kişisel verilerin aktarıldığı üçüncü kişilere bildirilmesini isteme,</p>
+            <p>·        işlenen verilerin münhasıran otomatik sistemler vasıtasıyla analiz edilmesi suretiyle kişinin kendisi aleyhine bir sonucun ortaya çıkmasına itiraz etme,</p>
+            <p>·        kişisel verilerin kanuna aykırı olarak işlenmesi sebebiyle zarara uğraması hâlinde zararın giderilmesini talep etme</p>
+            <p>haklarına sahiptir.</p>
+            <p>Söz konusu haklarınıza ilişkin taleplerinizi,<a href="../iletisim.php" target="_blank">iletişim sayfamızdan</a>  iletebilirsiniz. Talebinizin niteliğine göre en kısa sürede sonuçlandırılacaktır; ancak işlemin ayrıca bir maliyet gerektirmesi halinde Kişisel Verileri Koruma Kurulu tarafından belirlenecek tarifeye göre tarafınızdan ücret talep edilebilecektir.</p>
+            <p> </p>
+            
+            		</div>
+            	</div>
+            </div>
+            </div>
+            <div class="richtext parbase">
+            <div class="container-maxwidth">
+            	<div class="row">
+            		<div class="arc-richtext-editor-wrapper col-xs-12">
+            			
+            				<p><i>Son güncelleme tarihi: 09/06/2020</i><br />
+            </p>
+            
+            		</div>
+            	</div>
+            </div>
+        </section>
+    </section>
+</section>
+<?php include "inc/footer.php"; ?>
